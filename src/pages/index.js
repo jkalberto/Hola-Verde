eb74bde9a54ff9466a231c702a88dbfba697864a
Albert <ajk@q.com>
2021-04-19 13:10:08 -0600

    Gatsby website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `green` }}>
+      <h1>Hola Verde!</h1>
+      <p>What a world.</p>
+      <img src="C:\Users\viole\Hola-Verde\treesandfog.jpg"/>
+    </div>
+  );
 }
