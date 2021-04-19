fa23639df19e16fd9475fe7b1b392635feddddf4
Albert <ajk@q.com>
2021-04-19 17:51:15 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div style={{ color: `green` }}>
       <h1>Hola Verde!</h1>
       <p>What a world.</p>
-      <img src="C:\Users\viole\Hola-Verde\treesandfog.png" alt="why no picture"/>
+      <img src="C:\Users\viole\Hola-Verde\treesandfog.png" alt="why no pix"/>
     </div>
   );
 }
